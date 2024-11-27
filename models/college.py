cb0efac38299ed1f6f02329fbd6d5457d1bc7c1d
Nicole Cabansag <111856754+iskaiflakes@users.noreply.github.com>
2024-11-27 11:53:24 +0800

    added column to college table (color_code)

--- models/college.py
+++ models/college.py
@@ -4,3 +4,4 @@ class College(db.Model):
     __tablename__ = 'college'
     college_id = db.Column(db.String(6), primary_key=True)
     college_name = db.Column(db.String(50))
+    color_code = db.Column(db.String(10))
