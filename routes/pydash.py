a2d0dae11f0c82fd564ea4099dbf45ad56ac2679
Nicole Cabansag <111856754+iskaiflakes@users.noreply.github.com>
2024-12-09 10:49:22 +0800

    Fixed error in the query (pydash.py)

--- routes/pydash.py
+++ routes/pydash.py
@@ -1,9 +1,8 @@
-from flask import Blueprint, request, jsonify, json, redirect
-from models import db,Conference,Role, UserProfile,Program,College, Account
-from flask_jwt_extended import get_jwt_identity,jwt_required,get_jwt
+from flask import Blueprint, request, jsonify, redirect, session
+from models import db, Conference, Role, UserProfile, Program, College, Account
+from flask_jwt_extended import get_jwt_identity, jwt_required
 
-
-pydash = Blueprint('dash',__name__)
+pydash = Blueprint('dash', __name__)
 
 @pydash.route('/sampledash', methods=['GET'])
 @jwt_required()
@@ -12,31 +11,33 @@ def start_dash():
         user_id = get_jwt_identity()
 
         # Query the database for user profile
-        query = UserProfile.query.filter(UserProfile.researcher_id == user_id).join(Account, user_id== Account.user_id).first()
-        
+        account_info = db.session.query(Account).filter(Account.user_id == user_id).first()
+        user_prof = db.session.query(UserProfile).filter(UserProfile.researcher_id == user_id).first()
 
-        if not query:
+        if not account_info:
+            return jsonify({"error": "Account information not found"}), 404
+        
+        if not user_prof:
             return jsonify({"error": "User profile not found"}), 404
-        
-        print(f"user_id: {user_id}, role_id: {query.role_id}, email: {query.email}")
 
-        print(f"college_id: {query.college_id}, program_id: {query.program_id}")
+        print(f"user_id: {user_id}, role_id: {account_info.role_id}, email: {account_info.email}")
+        print(f"college_id: {user_prof.college_id}, program_id: {user_prof.program_id}")
 
         # Generate the full URL for the Dash app
         base_url = "http://localhost:5000"  # Adjust to your Dash app's URL
         
         # Dynamically create the query string with multiple parameters
         query_params = {
-            "user-role": query.role_id,
-            "college": query.college_id,
-            "program": query.program_id,
+            "user-role": account_info.role_id,
+            "college": user_prof.college_id,
+            "program": user_prof.program_id,
         }
 
         # Ensure all query parameters are valid (non-null)
         query_string = '&'.join(f"{key}={value}" for key, value in query_params.items() if value is not None)
 
-        if query.role_id =="02":
-            dash_url=f"{base_url}/dashboard/overview/"
+        if account_info.role_id == "02":
+            dash_url = f"{base_url}/dashboard/overview/"
         else:
             dash_url = f"{base_url}/sample/?{query_string}"
 
@@ -47,11 +48,3 @@ def start_dash():
 
     except Exception as e:
         return jsonify({"error": str(e)}), 500
-
-
-
-
-
-
-
-
