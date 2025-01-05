5f47d8a89a5ee922615c875d7954ac3c3a1af7b2
Deokgo <99870336+Deokgo@users.noreply.github.com>
2025-01-06 04:13:16 +0800

    KG Module
    - Added separate knowledge graph view for collection page (will direct to knowledge graph page once clicked)
    - Redesign knowledge graph page (aligned the view just like from added pages; will add more features)

--- knowledgegraph/collectionkg.py
+++ knowledgegraph/collectionkg.py
@@ -8,7 +8,7 @@ from flask import Flask
 from . import db_manager
 import pandas as pd
 
-def create_kg_sdg(flask_app):
+def collection_kg(flask_app):
     df = db_manager.get_all_data()
     df['date_approved'] = pd.to_datetime(df['date_approved'], errors='coerce')
     G = nx.Graph()
@@ -79,7 +79,7 @@ def create_kg_sdg(flask_app):
                 filtered_count = len([study for study in connected_nodes[node] if study in filtered_nodes])
                 hover_text.append(f"{filtered_count} studies connected")
                 node_color.append('#0A438F')
-                size = 60 + (filtered_count - min_connections) / (max_connections - min_connections) * (150 - 60) if max_connections > min_connections else 60
+                size = 30 + (filtered_count - min_connections) / (max_connections - min_connections) * (150 - 30) if max_connections > min_connections else 60
                 node_size.append(size * 0.75)
                 node_labels.append(node)
             else:
@@ -118,7 +118,7 @@ def create_kg_sdg(flask_app):
             y=[y + 0.05 for y in node_y],
             mode='text',
             text=node_labels,
-            textfont=dict(color='white', size=12),  # Shadow style
+            textfont=dict(color='white', size=8),  # Shadow style
             hoverinfo='none'
         )
 
@@ -129,7 +129,7 @@ def create_kg_sdg(flask_app):
             text=node_labels,
             hovertext=hover_text,
             marker=dict(color=node_color, size=node_size),
-            textfont=dict(color='black', size=12),  # Main text style
+            textfont=dict(color='black', size=9),  # Main text style
             hoverinfo='text'
         )
 
@@ -141,140 +141,56 @@ def create_kg_sdg(flask_app):
     initial_edges = list(G.edges())
     edge_trace, shadow_trace, node_trace = build_traces(initial_nodes, initial_edges, initial_nodes, show_labels=False)
     # Initialize Dash app
-    dash_app = Dash(__name__, server=flask_app, url_base_pathname='/knowledgegraph/')
+    dash_app = Dash(__name__, server=flask_app, url_base_pathname='/collectionkg/')
 
-    # Define styles as Python dictionaries
-    styles = {
-        'filter_container': {
-            'width': '25%',
-            'padding': '25px',
-            'border': "1px solid #0A438F",
-            'borderRadius': '14px',
-            'margin': '15px'
-        },
-        'graph_container': {
-            'width': '100%',
-        },
-        'main_container': {
-            'display': 'flex',
-            'flexDirection': 'row',
-            'width': '100%',
-            'height': '100%',
-            'fontFamily': 'Montserrat',
-            'backgroundColor': 'white',
-        },
-        'slider_container': {
-            'width': 'inherit',
-            'padding': '5px',
-            'fontSize' : '10px',
-            'color': '#08397C',
-            'marginBottom': '20px'
-        },
-        'dropdown_container': {
-            'width': 'inherit',
-            'fontSize' : '13px',
-            'padding': '5px',
-            'marginBottom': '20px'
-        },
-        'filter_button': {
-            'width': '100%',
-            'backgroundColor': '#08397C',
-            'fontSize' : '15px',
-            'color': 'white',
-            'padding': '10px',
-            'borderRadius': '4px',
-            'border': 'none',
-            'cursor': 'pointer'
-        },
-        'label': {
-            'marginBottom': '8px',
-            'fontSize' : '13px',
-            'color': '#08397C',
-            'fontFamily': 'Montserrat',
-            'display': 'block'
-        },
-        'main_label': {
-            'fontSize' : '20px',
-            'color': '#F40824',
-            'fontFamily': 'Montserrat',
-            'fontWeight': 'bold',
-            'marginBottom': '16px',
-            'display': 'block'
-        }
-    }
-
-    # Updated layout with inline styles
+    # Layout for Dash app including filters for year and colleges
     dash_app.layout = html.Div([
-        # Container for filters
-        html.Div([
-            html.Label('Filters', style=styles['main_label']),
-            html.Div([
-                html.Label('Select Year Range:', style=styles['label']),
-                html.Div([
-                    dcc.RangeSlider(
-                        id='year-slider',
-                        min=df['date_approved'].dt.year.min(),
-                        max=df['date_approved'].dt.year.max(),
-                        value=[df['date_approved'].dt.year.min(), df['date_approved'].dt.year.max()],
-                        marks={year: str(year) for year in range(df['date_approved'].dt.year.min(), df['date_approved'].dt.year.max() + 1, 2)},
-                        step=1
-                    )
-                ], style=styles['slider_container']),
-            ]),
-            
-            html.Div([
-                html.Div([
-                    html.Label('Select Colleges:', style=styles['label']),
-                    html.Div([
-                        dcc.Dropdown(
-                            id='college-dropdown',
-                            options=[{'label': college, 'value': college} for college in df['college_id'].unique()],
-                            value=[],
-                            multi=True,
-                            placeholder='Select colleges...'
-                        )
-                    ], style=styles['dropdown_container']),
-                ]),
-
-                html.Button(
-                    'Apply Filters',
-                    id='apply-filters',
-                    n_clicks=0,
-                    style=styles['filter_button']
-                )
-            ])        
-        ], style=styles['filter_container']),
-        
-        # Container for graph
         html.Div([
             dcc.Graph(
                 id='knowledge-graph',
                 figure={
                     'data': [edge_trace, shadow_trace, node_trace],
                     'layout': go.Layout(
-                        title='<br>Research Studies Knowledge Graph (Overall View)',
-                        titlefont=dict(size=16),
                         showlegend=False,
                         hovermode='closest',
-                        margin=dict(b=0, l=0, r=0, t=25),
-                        xaxis=dict(showgrid=False, zeroline=False),
-                        yaxis=dict(showgrid=False, zeroline=False),
+                        margin=dict(b=0, l=0, r=0, t=0),
+                        autosize=True,
+                        xaxis=dict(
+                            showgrid=False, 
+                            zeroline=False,
+                            range=[-3, 3],  # Fixed range for x-axis
+                            fixedrange=True   # Prevents zoom on x-axis
+                        ),
+                        yaxis=dict(
+                            showgrid=False, 
+                            zeroline=False,
+                            range=[-3, 3],  # Fixed range for y-axis
+                            fixedrange=True   # Prevents zoom on y-axis
+                        ),
                         transition=dict(duration=500),
                     )
                 },
                 style={
-                    'height': 'calc(100vh - 40px)',
-                    'width': '100%'
+                    'height': '100%',
+                    'width': '100%',
                 },
                 config={
-                    'responsive': True,
-                    'scrollZoom': True,
-                    'displayModeBar': True,
-                    'modeBarButtonsToRemove': ['lasso2d', 'select2d']
+                    'displayModeBar': False,  # Hide the mode bar
+                    'staticPlot': True,  # Make the plot completely static
                 }
             )
-        ], style=styles['graph_container'])
-    ], style=styles['main_container'])
+        ], style={
+            'width': '100%',
+            'display': 'flex',
+            'justifyContent': 'center',
+        })
+    ], style={
+        'width': '100%',
+        'height': '100%',
+        'display': 'flex',
+        'justifyContent': 'center',
+        'alignItems': 'center',
+    })
 
     # Callback to handle initial graph display, filters, and node click events
     @dash_app.callback(
@@ -329,7 +245,6 @@ def create_kg_sdg(flask_app):
                     edge for edge in G.edges(clicked_node)
                     if edge[1] in filtered_nodes  # Respect current filters
                 ]
-                new_title = f'<br>Research Studies Knowledge Graph - {clicked_node}'
                 show_labels = True
 
         edge_trace, shadow_trace, node_trace = build_traces(nodes_to_show, edges_to_show, filtered_nodes, show_labels=show_labels)
@@ -341,10 +256,11 @@ def create_kg_sdg(flask_app):
                 showlegend=False,
                 hovermode='closest',
                 margin=dict(b=0, l=0, r=0, t=25),
+                width=1300,  # Increased width
+                height=1200,  # Increased height
                 xaxis=dict(showgrid=False, zeroline=False),
                 yaxis=dict(showgrid=False, zeroline=False),
                 transition=dict(duration=500),  
-                
             )
         }
         return new_figure
--- server.py
+++ server.py
@@ -113,6 +113,7 @@ from dashboards.main_dashboard import create_main_dashboard
 from dashboards.main_dash import MainDashboard
 from dashboards.pub_dash import PublicationDash
 from knowledgegraph.knowledgegraph import create_kg_sdg
+from knowledgegraph.collectionkg import collection_kg
 from dashboards.college_dash import CollegeDashApp
 from dashboards.program_dash import ProgDashApp
 import dash_bootstrap_components as dbc
@@ -133,6 +134,7 @@ def create_dash_apps(app):
             PublicationDash(app)
             create_main_dashboard(app)
             create_kg_sdg(app)
+            collection_kg(app)
             CollegeDashApp(app)
             ProgDashApp(app)
             SDG_Dash(app)
