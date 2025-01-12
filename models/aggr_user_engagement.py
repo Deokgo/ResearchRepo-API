d1aee555e2552ef8efc42c6e5faed1d9480cf404
Nicole Cabansag <111856754+iskaiflakes@users.noreply.github.com>
2025-01-12 18:26:05 +0800

    Modified aggr_user_engagement model

--- models/aggr_user_engagement.py
+++ models/aggr_user_engagement.py
@@ -2,8 +2,9 @@ from . import db
 
 class AggrUserEngagement(db.Model):
     __tablename__ = 'aggr_user_engagement'
-    research_id = db.Column(db.String(15), db.ForeignKey('research_outputs.research_id'), primary_key=True)
-    day = db.Column(db.Date, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    research_id = db.Column(db.String(15), db.ForeignKey('research_outputs.research_id'))
+    day = db.Column(db.Date)
     total_views = db.Column(db.Integer)
     total_downloads = db.Column(db.Integer)
     unique_views = db.Column(db.Integer)
